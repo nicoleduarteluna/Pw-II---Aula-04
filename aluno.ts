c95c522013b3f0181f46ffe14f4a8b9b3f95e369
niluiz <nicoleduarteluna@hotmail.com>
2019-08-28 20:15:13 -0300

    Criação da Classe Aluno

--- aluno.ts
+++ aluno.ts
@@ -1,3 +1,11 @@
 class Aluno {
-    
+   
+    // o constructor não é obrigatório, porém no angular é muito utilizado 
+    // "string:nome" fica oculto quando se usa o constructor
+    constructor (public nome:string) {}
+
+    exibir () {
+        console.log("Nome:" + this.nome)
+        
+    }
 }
